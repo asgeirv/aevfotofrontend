1bb443c60dd15c739f6454e582e448e2d3a1c43f
Asgeir Vinkenes <asgeirv@protonmail.com>
2026-01-13 15:54:24 +0100

    Create basic UI and allow clicking back and forth between photos

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Image} from "primereact/image";
+import {Card} from 'primereact/card';
+import {Dropdown} from 'primereact/dropdown';
+import {Button} from 'primereact/button';
+import {Rating} from 'primereact/rating';
+import {FloatLabel} from 'primereact/floatlabel';
+import "primereact/resources/themes/bootstrap4-dark-purple/theme.css";
+import 'primeicons/primeicons.css';
+
+import {useState, useEffect} from 'react';
+import type {Photo} from "./models/Photo.ts";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="root">
+            <div id="date-nav-container">
+                <Card>
+                    <div className="datepicker-container">
+                        <FloatLabel>
+                            <Dropdown inputId="year-picker"/>
+                            <label htmlFor="year-picker">Year</label>
+                        </FloatLabel>
+                    </div>
+                    <div className="datepicker-container">
+                        <FloatLabel>
+                            <Dropdown inputId="month-picker"/>
+                            <label htmlFor="month-picker">Month</label>
+                        </FloatLabel>
+                    </div>
+                    <div className="datepicker-container">
+                        <FloatLabel>
+                            <Dropdown inputId="subfolder-picker"/>
+                            <label htmlFor="subfolder-picker">Subfolder</label>
+                        </FloatLabel>
+                    </div>
+                </Card>
+            </div>
+            <PhotoView/>
+        </div>
+    );
+}
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function PhotoView() {
+    const [photoIds, setPhotoIds] = useState<number[]>();
+    useEffect(() => {
+        fetch('http://localhost:8080/api/photos')
+            .then(res => res.json())
+            .then(data => setPhotoIds(data))
+            .catch(err => console.log(err));
+    })
+    const [currentPhoto, setCurrentPhoto] = useState<number>(0);
+
+    function previousPhoto() {
+        if (currentPhoto === 0) {
+            setCurrentPhoto(photoIds?.length - 1);
+        } else {
+            setCurrentPhoto(currentPhoto - 1);
+        }
+    }
+
+    function nextPhoto() {
+        if (currentPhoto === photoIds?.length - 1) {
+            setCurrentPhoto(0);
+        } else {
+            setCurrentPhoto(currentPhoto + 1);
+        }
+    }
+
+    function deletePhoto() {
+
+    }
+
+    return (
+        <div id="image-container">
+            <Card>
+                {photoIds === undefined ? (
+                    <p>No photos found!</p>
+                ) : (
+                    <>
+                        <p>{currentPhoto + 1}/{photoIds.length}</p>
+                        <Image src={"http://localhost:8080/api/photos/" + photoIds[currentPhoto]} width="100%"/>
+                        <div id="photo-nav-container">
+                            <Button icon="pi pi-arrow-left"
+                                    onClick={previousPhoto}/>
+
+                            <Rating/>
+
+                            <Button icon="pi pi-arrow-right"
+                                    onClick={nextPhoto}/>
+                        </div>
+                        <div id="photo-del-container">
+                            <Button icon="pi pi-trash"
+                                    severity="danger"
+                                    onClick={deletePhoto}/>
+                        </div>
+                    </>
+                )}
+            </Card>
+        </div>
+    )
 }
 
 export default App
+
---
+++ src/models/Photo.ts
@@ -0,0 +1,6 @@
+export type Photo = {
+    id: number;
+    name: string;
+    rating: number;
+    flaggedForDeletion: boolean;
+}
