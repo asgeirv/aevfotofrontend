eefa5b3da3f6edde1951f2292585e352dc9a97cd
Asgeir Vinkenes <asgeirv@protonmail.com>
2026-01-27 10:48:22 +0100

    Add functionality for deleting all photos marked for deletion

--- src/components/DeletionView.tsx
+++ src/components/DeletionView.tsx
@@ -3,6 +3,7 @@ import {useEffect, useRef, useState} from "react";
 import {OverlayPanel} from "primereact/overlaypanel";
 import type {Photo} from "../models/Photo.ts";
 import {PhotoTable} from "./PhotoTable.tsx";
+import {confirmDialog, ConfirmDialog} from "primereact/confirmdialog";
 
 export function DeletionView() {
     const deleted = useRef<OverlayPanel>(null);
@@ -15,6 +16,28 @@ export function DeletionView() {
             .catch((err) => console.log(err));
     }, []);
 
+    const accept = () => {
+        fetch(`http://localhost:8080/api/photos/deleted/nuke`,
+            {
+                method: "DELETE",
+            })
+            .then(() => setPhotos([]))
+            .catch((err) => console.log(err));
+    };
+
+    const reject = () => {};
+
+    const confirmNuke = () => {
+        confirmDialog({
+            message: "Are you sure you want to permanently delete all photos marked for deletion?",
+            header: "Confirm deletion",
+            icon: "pi pi-exclamation-triangle",
+            defaultFocus: "reject",
+            accept,
+            reject
+        })
+    }
+
     return (
         <div>
             <Button type="button"
@@ -25,10 +48,12 @@ export function DeletionView() {
                 <PhotoTable photos={photos}
                             emptyMessage={"No photos flagged for deletion."}/>
 
+                <ConfirmDialog/>
                 <Button type="button"
                         icon="pi pi-trash"
                         severity="danger"
                         size="large"
+                        onClick={confirmNuke}
                         style={{width: "100%"}}/>
             </OverlayPanel>
         </div>
