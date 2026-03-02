32f29d65c59d166b1b02d7f95b59b64e2ff2d75c
Asgeir Vinkenes <asgeirv@protonmail.com>
2026-03-02 09:59:09 +0100

    Prep for HTTPS

--- src/LoginPage.tsx
+++ src/LoginPage.tsx
@@ -1,9 +1,9 @@
+import * as React from "react";
 import {useState} from "react";
 import {Card} from "primereact/card";
 import {Button} from "primereact/button";
 import {FloatLabel} from "primereact/floatlabel";
 import {InputText} from "primereact/inputtext";
-import * as React from "react";
 import {useNavigate} from "react-router-dom";
 
 export default function LoginPage() {
